ab72577d5545b7b6ae8e2627abd1cff62e70199e
nidhinkumar <nidhinkumar@codingtown.com>
2018-07-05 16:13:28 +0530

    add battery image

--- Progress.js
+++ Progress.js
@@ -1,24 +1,35 @@
 /* @flow */
 
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Button,
+  Image
+} from "react-native";
 import ProgressBarAnimated from "react-native-progress-bar-animated";
 
 export default class Progress extends Component {
   state = {
-    progress: 100,
+    progress: 95,
     progressWithOnComplete: 0,
     progressCustomized: 0
   };
   increase = (key, value) => {
     this.setState({ [key]: this.state[key] + value });
   };
+  decrease = (key, value) => {
+    this.setState({ [key]: this.state[key] - value });
+  };
   render() {
-    console.log("Progrss bar check");
+    console.log("Progrss bar");
     const barWidth = Dimensions.get("screen").width - 30;
+    const batteryWidth = Dimensions.get("screen").width;
     const progressCustomStyles = {
       backgroundColor: "red",
-      borderRadius: 10,
+      borderRadius: 0,
       borderColor: "orange"
     };
     return (
@@ -28,9 +39,35 @@ export default class Progress extends Component {
             width={barWidth}
             value={this.state.progress}
             height={150}
-            backgroundColorOnComplete="green"
+            borderRadius={0}
+            style={{ zIndex: 0 }}
+            backgroundColor={
+              this.state.progress > 20 && this.state.progress < 90
+                ? "yellow"
+                : this.state.progress >= 90
+                  ? "green"
+                  : "red"
+            }
           />
+          <Image
+            style={{
+              width: batteryWidth,
+              height: 150,
+              position: "absolute",
+              zIndex: 1
+            }}
+            source={require("./assets/battery.png")}
+          />
+          {/* <Button
+            title="Increase 10%"
+            onPress={this.increase.bind(this, "progress", 10)}
+          /> */}
         </View>
+        {/* <Button
+          title="Decrease 10%"
+          onPress={this.decrease.bind(this, "progress", 10)}
+        /> */}
+        {/* <Text>{this.state.progress}</Text> */}
       </View>
     );
   }
@@ -39,7 +76,8 @@ export default class Progress extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center"
+    justifyContent: "center",
+    backgroundColor: "#FFFFFF"
   },
   angle: {
     transform: [{ rotate: "-90deg" }]
